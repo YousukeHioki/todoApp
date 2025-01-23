f193a46b3340b540ceb5da4315796440e40eb148
YousukeHioki <yousuke_hioki@mail.toyota.co.jp>
2025-01-24 08:55:45 +0900

    made crud func post and get

--- src/main/kotlin/com/example/todoapp/TodoController.kt
+++ src/main/kotlin/com/example/todoapp/TodoController.kt
@@ -1,56 +1,124 @@
 package com.example.todoapp
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest
+import java.awt.SystemColor.text
+import java.net.URI
+import java.util.*
+import java.util.HashMap
+import java.util.Map
 
-data class Todo (
-    var id: String,
-    var todo: String,
-    var isFinished: Boolean
-)
+class TodoRequest {
+    var  text: String = ""
+}
+class TodoItem {
+    var PK: String = ""
+    var text: String = ""
+    //printした時にデータをちゃんと表示させるためだけの処理
+    override fun toString(): String {
+        return "TodoItem(PK='$PK', text='$text')"
+    }
+}
 
-val todoRepository = listOf(
-    Todo(
-        id = "12345",
-        todo = "studyAboutSQL",
-        isFinished = false
-    ),
-    Todo(
-        id = "67890",
-        todo = "studyAboutDB",
-        isFinished = true
-    )
-)
 
+//data class Todo (
+//    var id: String,
+//    var todo: String,
+//    var isFinished: Boolean
+//)
+//
+//val todoRepository = listOf(
+//    Todo(
+//        id = "12345",
+//        todo = "studyAboutSQL",
+//        isFinished = false
+//    ),
+//    Todo(
+//        id = "67890",
+//        todo = "studyAboutDB",
+//        isFinished = true
+//    )
+//)
 
 @RestController
 class TodoController {
 
+//    @GetMapping("/todo/{id}")
+//    fun getTodoById(@PathVariable id: String): Boolean {
+//        //!!!kotlinでは対象のデータをitで表す
+//        //!!!kotlinでは===は参照の等価性を、==はオブジェクトの内容の等価性をチェックする
+//        val todoItem = todoRepository.find { it.id == id }
+//        //printlnで改行して次を表示、printのみでは横並びで繋がって表示され見にくくなる
+//        print("todoRepositoryを表示-------")
+//        println(todoRepository)
+//        println("todoItemを表示-------")
+//        println(todoItem)
+//        println("todoItem.isFinishedを表示-------${todoItem!!.isFinished}")
+//
+//        return todoItem.isFinished
+//    }
+
     @PostMapping("/todo")
-    fun addTodoItem(): String {
-        return "OK"
+    fun addNewItem(@RequestBody todo: TodoRequest) {
+        //追加したいアイテム
+        val item = mapOf(
+            //ランダムなUUIDをPKに入れる、この場合は.toString()が必要
+            "PK" to AttributeValue.fromS(UUID.randomUUID().toString()),
+            "text" to AttributeValue.fromS(todo.text)
+        )
+        //DBと接続
+        val client = DynamoDbClient.builder()
+            .endpointOverride(URI.create("http://localhost:4566"))
+            .credentialsProvider(AnonymousCredentialsProvider.create())
+            .region(Region.AP_NORTHEAST_1)
+            .build()
+        //アイテムを追加するリクエスト
+        val putItemRequest = PutItemRequest.builder()
+            .tableName("test")
+            .item(item)
+            .build()
+        client.putItem(putItemRequest)
+        //追加された状態のテーブルデータを取得
+        val scanItemRequest = ScanRequest.builder()
+            .tableName("test")
+            .build()
+        val response = client.scan(scanItemRequest)
+        val items = response.items().toList()
+        println(items)
+
     }
 
-    @GetMapping("/todo/{id}")
-    fun getTodoById(@PathVariable id: String): Boolean {
-        //!!!kotlinでは対象のデータをitで表す
-        //!!!kotlinでは===は参照の等価性を、==はオブジェクトの内容の等価性をチェックする
-        val todoItem = todoRepository.find { it.id == id }
-        //printlnで改行して次を表示、printのみでは横並びで繋がって表示され見にくくなる
-        println("todoRepository全て表示↓")
-        println(todoRepository)
-        println("todoRepositoryの一つ目のみ表示↓")
-        println(todoRepository[0])
-        println("todoItemを表示↓")
-        println(todoItem)
-        println("todoItem!!.isFinishedを表示↓")
-        //TODO：!!はnullが入らないようにするっぽい、?の場合は何か調べる
-        println(todoItem!!.isFinished)
-
-
-        return todoItem.isFinished
+    @GetMapping("/todo")
+    fun getAllItems(): List<TodoItem> {
+        val client = DynamoDbClient.builder()
+            .endpointOverride(URI.create("http://localhost:4566")) //テスト時などに特定のEPを指定する
+//          →参考---https://docs.aws.amazon.com/ja_jp/sdk-for-java/latest/developer-guide/region-selection.html
+            .credentialsProvider(AnonymousCredentialsProvider.create()) // 匿名認証のため必要
+//          →参考---https://docs.aws.amazon.com/ja_jp/sdk-for-java/latest/developer-guide/client-creation-defaults.html
+            .region(Region.AP_NORTHEAST_1) //利用位置から近い地域を設定
+            .build()
+        val request = ScanRequest.builder()
+            .tableName("test")
+            .build()
+        val response = client.scan(request)
+        val items = response.items().toList()
+        val resultItems = mutableListOf<TodoItem>()
+        items.forEach { item ->
+            val resultPK = item["PK"]?.s() ?: ""
+            val resultText = item["text"]?.s() ?: ""
+            //TodoItemを初期化し新しいTodoItemを作成
+            val resultItem = TodoItem().apply {
+                PK = resultPK
+                text = resultText
+        }
+            resultItems.add(resultItem)
+        }
+        println("resultItems-------$resultItems")
+        return resultItems
     }
-
 }
--- src/test/kotlin/com/example/todoapp/TodoappApplicationTests.kt
+++ src/test/kotlin/com/example/todoapp/TodoappApplicationTests.kt
@@ -6,21 +6,71 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest
+
+
+import java.net.URI
+import java.util.*
+
+class Todo {
+	var id: String = ""
+	var text: String = ""
+}
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class TodoappApplicationTests {
 
+	private val client = DynamoDbClient.builder()
+		.endpointOverride(URI.create("http://localhost:4566"))
+		.credentialsProvider(AnonymousCredentialsProvider.create())
+		.region(Region.AP_NORTHEAST_1)
+		.build()
+
+	fun deleteAllItems(tableName: String){
+
+		val request = ScanRequest.builder()
+			.tableName(tableName)
+			.build()
+		//this.は省略可能
+		val beforeResponse = client.scan(request)
+		val beforeItems = beforeResponse.items().toList()
+
+		for (item in beforeItems) {
+			val deleteRequest = DeleteItemRequest.builder()
+				.tableName(tableName)
+				.key(mapOf("PK" to item["PK"])) // .key({ PK: item.PK })
+				.build()
+			client.deleteItem(deleteRequest)
+		}
+	}
+
+	fun scanAllItems(tableName: String): List<Map<String, AttributeValue>>{
+		val request = ScanRequest.builder()
+			.tableName("test")
+			.build()
+		val response = client.scan(request)
+		val items = response.items().toList()
+		return items
+	}
+
 	@Autowired
 	private lateinit var mockMvc: MockMvc
 
-	@Test
-	fun contextLoads() {
-	}
+//	@Test
+//	fun contextLoads() {
+//	}
 
 
 	@Test
@@ -30,22 +80,99 @@ class TodoappApplicationTests {
 
 	@Test
 	fun `エンドポイントtodoにPOSTすると 200 OK が返る`(){
-		mockMvc.perform(post("/todo"))
+		mockMvc.perform(post("/todo").content("{\"text\":\"bar\"}")
+			.contentType(MediaType.APPLICATION_JSON)
+		)
 			.andExpect(status().isOk)
 	}
 
 	@Test
-	fun `todoエンドポイントに any をPOSTするとテーブルに any が追加される`() {
-		mockMvc.perform(post("/todo").content("{test:\"any\"}"))
+	fun `todoエンドポイントに foo をPOSTするとテーブルに foo が追加される`() {
+		// Setup
+		//以下のコードでDBテストする場合はbuild.gradle.ktsにimplementationする必要あり
+		deleteAllItems("test")
+
+		// Action
+		mockMvc.perform(post("/todo").content("{\"text\":\"bar\"}")
+			.contentType(MediaType.APPLICATION_JSON)
+		)
+
+		// Check
+		val request = ScanRequest.builder()
+			.tableName("test")
+			.build()
+		val afterResponse = client.scan(request)
+		val afterItems = afterResponse.items().toList()
+		assertThat(afterItems.size, equalTo(1))
 	}
 
 	@Test
-	fun `id のパスパラメータを渡すと id にあてはまるアイテムの完了状態が返る`() {
-		mockMvc.perform(get("/todo/{id}", "12345"))
-			.andExpect(status().isOk)
-			//データの特定の値を確認する
-			.andExpect(content().string("false"))
-			//モックの値？が表示されるので"false"が表示されるわけではない
-			println(content().string("false"))
+	fun `todoエンドポイントに bar をPOSTするとテーブルに bar が追加される`() {
+		// Setup
+		//以下のコードでDBテストする場合はbuild.gradle.ktsにimplementationする必要あり
+		deleteAllItems("test")
+
+		// Action
+		mockMvc.perform(post("/todo").content("{\"text\":\"bar\"}")
+			.contentType(MediaType.APPLICATION_JSON)
+		)
+		// Check
+		val request = ScanRequest.builder()
+			.tableName("test")
+			.build()
+		val afterResponse = client.scan(request)
+		val afterItems = afterResponse.items().toList()
+		assertThat(afterItems.size, equalTo(1))
 	}
+
+	@Test
+	fun `二つPOSTしたときに それぞれ違うIDで登録させれている`(){
+		//Setup
+		deleteAllItems("test")
+
+		// Action
+		mockMvc.perform(post("/todo").content("{\"text\":\"bar\"}")
+			.contentType(MediaType.APPLICATION_JSON)
+		)
+		mockMvc.perform(post("/todo").content("{\"text\":\"foo\"}")
+			.contentType(MediaType.APPLICATION_JSON)
+		)
+		//Check
+		val allItems = scanAllItems("test")
+		assertThat(allItems.size, equalTo(2))
+	}
+
+	@Test
+	fun `todoエンドポイントに GET をリクエストして全てのテーブル情報を返す`(){
+		//Setup
+		deleteAllItems("test")
+
+		val item = mapOf(
+			"PK" to AttributeValue.fromS(UUID.randomUUID().toString()),
+			"text" to AttributeValue.fromS("12345")
+		)
+		val putItemRequest = PutItemRequest.builder()
+			.tableName("test")
+			.item(item)
+			.build()
+		client.putItem(putItemRequest)
+
+		mockMvc.perform(get("/todo"))
+			.andExpect(status().isOk)
+			.andExpect(jsonPath("$.length()").value(1))
+			.andExpect(jsonPath("$[0].text").value("12345"))
+	}
+
+//	@Test
+//	fun `id のパスパラメータを渡すと id にあてはまるアイテムの完了状態が返る`() {
+//		val result = mockMvc.perform(get("/todo/{id}", "12345"))
+//			.andExpect(status().isOk)
+//			//データの特定の値を確認する
+//			//.andExpect(content().string("false"))
+//			//Booleanで値を取得する方法↓
+////			.andExpect(jsonPath("$.isFinished").value(false))
+//			.andReturn()
+//			result.response.contentAsString
+//			println("response-------${result.response.contentAsString}")
+//	}
 }
