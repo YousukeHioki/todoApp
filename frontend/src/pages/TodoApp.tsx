567beafdc0a86393775383dab320477c4233c5c4
YousukeHioki <yousuke_hioki@mail.toyota.co.jp>
2025-01-26 06:01:30 +0900

    made test to fetch backend

--- frontend/src/pages/TodoApp.tsx
+++ frontend/src/pages/TodoApp.tsx
@@ -1,4 +1,27 @@
+import { useEffect } from "react";
+
 export function TodoApp() {
+    useEffect(() => {
+        (async () => {
+            await getAllTodoItems();
+        })();
+    }, []);
+
+    async function getAllTodoItems() {
+        const fetchResponse: Response = await fetch(
+            "http://localhost:8080/todo",
+        );
+        console.log("fetchResponse", fetchResponse); //OK
+
+        if (fetchResponse.ok) {
+            const jsonResponse = await fetchResponse.json();
+            console.log("allTodoItems-----", jsonResponse);
+        } else {
+            console.log("NG-----", fetchResponse.status);
+        }
+        console.log("最後まできた");
+    }
+
     return (
         <>
             <h1>MAIN PAGE</h1>
